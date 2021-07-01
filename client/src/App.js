86dfea8a03df28a2913b05bf9840cb11dca08a01
ankitdubey9329 <ankitdubey313@gmail.com>
2021-07-01 17:39:29 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component,useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Formuser from './components/form';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div>
+      
+      <Formuser/>
+    </div>
+  )
+}
+
+
+
+
+export default App;
